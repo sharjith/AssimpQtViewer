989e75eaeceba166b238a3760fc1a610b8c47afc
Sharjith Naramparambath <sharjith.com>
2025-05-15 13:36:00 +0530

    Removed build folder

--- build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AssimpQtViewer_FILE D:/work/progs/Qt6/AssimpQtViewer/build/Debug/AssimpQtViewer.exe)
-set(__QT_DEPLOY_TARGET_AssimpQtViewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AssimpQtViewer_RUNTIME_DLLS C:/Qt/6.8.2/msvc2022_64/bin/Qt6OpenGLWidgetsd.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6OpenGLd.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Software/libs/assimp-5.4.3/bin/assimp-vc143-mtd.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Cored.dll)
--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AssimpQtViewer_FILE D:/work/progs/Qt6/AssimpQtViewer/build/MinSizeRel/AssimpQtViewer.exe)
-set(__QT_DEPLOY_TARGET_AssimpQtViewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AssimpQtViewer_RUNTIME_DLLS C:/Qt/6.8.2/msvc2022_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6OpenGL.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Widgets.dll;D:/Software/libs/assimp-5.4.3/bin/assimp-vc143-mtd.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Core.dll)
