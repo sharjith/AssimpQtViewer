a2406e9c1283026e4deb47b7d831a5e3e26ea3f6
Sharjith Naramparambath <sharjith@gmail.com>
2025-05-26 09:03:59 +0530

    Added exit menu

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -69,6 +69,9 @@ MainWindow::MainWindow(QWidget* parent)
 		if (!filePath.isEmpty()) loadModel(filePath);
 		});
 
+	QAction* exitAct = fileMenu->addAction("Exit");
+	connect(exitAct, &QAction::triggered, this, &QWidget::close);
+
 	connect(_searchBox, &QLineEdit::textChanged, this, &MainWindow::filterTree);
 }
 
