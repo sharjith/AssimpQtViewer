2f9ee9e6d2f0aafa1834b9189123a2884bfc101b
Sharjith Naramparambath <sharjith.com>
2025-05-26 15:59:13 +0530

    Changed deprecated method

--- ModelViewerWidget.cpp
+++ ModelViewerWidget.cpp
@@ -227,7 +227,7 @@ GLuint ModelViewerWidget::loadTextureIfNeeded(const aiMaterial* material, unsign
             QString fullPath = QDir(m_lastModelPath).filePath(QString::fromUtf8(texturePath.C_Str()));
             QImage image(fullPath);
             if (!image.isNull()) {
-                image = image.convertToFormat(QImage::Format_RGBA8888).mirrored(false, true);
+                image = image.convertToFormat(QImage::Format_RGBA8888).flipped();
                 GLuint texId;
                 glGenTextures(1, &texId);
                 glBindTexture(GL_TEXTURE_2D, texId);
